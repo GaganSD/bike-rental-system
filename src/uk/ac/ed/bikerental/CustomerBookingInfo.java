068cd6050af86673fef20d64aadc3f4ab7679198
GaganSD <gagandevagiri@gmail.com>
2019-11-20 00:29:50 +0000

    complete CustomerBookingInfo class

---
+++ src/uk/ac/ed/bikerental/CustomerBookingInfo.java
@@ -0,0 +1,24 @@
+public class CustomerBookingInfo {
+
+    public String firstName;
+    public String phoneNum;
+    public String postCode;
+    public String address;
+    public String surname;
+    public String collectionMethod;
+
+
+    public CustomerBookingInfo(String firstName,String phoneNum,
+                String postCode, String address,String surname,String collectionMethod) {
+
+        this.firstName = firstName;
+        this.phoneNum = phoneNum;
+        this.postCode = postCode;
+        this.address = address;
+        this.surname = surname;
+        this.collectionMethod = collectionMethod;
+    }
+
+
+
+}
