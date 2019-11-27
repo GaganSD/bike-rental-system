f59130444ee0809cb0fc6e0aa3a1c3ec34fc6dc4
romanakash <romanakash2000@gmail.com>
2019-11-27 18:46:58 +0000

    java docs

--- src/uk/ac/ed/bikerental/DateRange.java
+++ src/uk/ac/ed/bikerental/DateRange.java
@@ -5,30 +5,69 @@ import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 import java.util.function.BooleanSupplier;
 
+/**
+ * DateRange is the class we'll be using to represent the period during which a
+ * bike can be booked
+ */
 public class DateRange {
+    /**
+     * private fields that store the start day and end day of the date range
+     */
     private LocalDate start, end;
 
+    /**
+     * constructor for our DateRange class
+     * 
+     * @param start the start date
+     * @param end   the end date
+     */
     public DateRange(LocalDate start, LocalDate end) {
         this.start = start;
         this.end = end;
     }
 
+    /**
+     * getter for start attribute
+     * 
+     * @return the start date
+     */
     public LocalDate getStart() {
         return this.start;
     }
 
+    /**
+     * getter for end attribute
+     * 
+     * @return the end date
+     */
     public LocalDate getEnd() {
         return this.end;
     }
 
+    /**
+     * convert date range to years
+     * 
+     * @return no. of years between the start and end date
+     */
     public long toYears() {
         return ChronoUnit.YEARS.between(this.getStart(), this.getEnd());
     }
 
+    /**
+     * convert date range to days
+     * 
+     * @return no. of days between the start and end date
+     */
     public long toDays() {
         return ChronoUnit.DAYS.between(this.getStart(), this.getEnd());
     }
 
+    /**
+     * checks if this date range overlaps with another date range
+     * 
+     * @param other DateRange to be checked against
+     * @return whther it overlaps or not
+     */
     public Boolean overlaps(DateRange other) {
         if (other.getStart().equals(this.start)) {
             return true;
@@ -47,19 +86,30 @@ public class DateRange {
         }
     }
 
+    /**
+     * hashes the object so it can be used in collections
+     * 
+     * @return hash of the object
+     */
     @Override
     public int hashCode() {
         // hashCode method allowing use in collections
         return Objects.hash(end, start);
     }
 
+    /**
+     * compares if this date range is equal to another object
+     * 
+     * @param Object to compare against
+     * @return true if both start and end are same for both objects
+     */
     @Override
     public boolean equals(Object obj) {
         // equals method for testing equality in tests
-        if (obj == this){
+        if (obj == this) {
             return true;
-        } else if (obj == null){
-            return false; 
+        } else if (obj == null) {
+            return false;
         } else if (getClass() != obj.getClass()) {
             return false;
         }
--- src/uk/ac/ed/bikerental/LinearDeprecation.java
+++ src/uk/ac/ed/bikerental/LinearDeprecation.java
@@ -4,12 +4,13 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class LinearDeprecation implements ValuationPolicy {
-    
+
     public BigDecimal calculateValue(Bike bike, LocalDate date) {
 
         int deprecationRate = 20;
 
         LocalDate today = LocalDate.now();
 
+        return new BigDecimal(10);
     }
 }
