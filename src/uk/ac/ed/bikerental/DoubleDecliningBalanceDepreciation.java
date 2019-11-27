44323ca42a89b0afdd7554b1d34c002187db8cb5
GaganSD <gagandevagiri@gmail.com>
2019-11-27 15:32:05 +0000

    half baked extensions

---
+++ src/uk/ac/ed/bikerental/DoubleDecliningBalanceDepreciation.java
@@ -0,0 +1,12 @@
+package uk.ac.ed.bikerental;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class DoubleDecliningBalanceDepreciation implements ValuationPolicy {
+    
+    public BigDecimal calculateValue(Bike bike, LocalDate date) {
+        
+        
+    }
+}
--- src/uk/ac/ed/bikerental/LinearDeprecation.java
+++ src/uk/ac/ed/bikerental/LinearDeprecation.java
@@ -3,9 +3,13 @@ package uk.ac.ed.bikerental;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class CustomValuation implements ValuationPolicy {
+public class LinearDeprecation implements ValuationPolicy {
     
     public BigDecimal calculateValue(Bike bike, LocalDate date) {
+
+        int deprecationRate = 20;
+
+        LocalDate today = LocalDate.now();
 
     }
 }
