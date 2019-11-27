d4d9951748b5ceb57db58fe9a6dbae2d2642273a
GaganSD <gagandevagiri@gmail.com>
2019-11-27 19:03:38 +0000

    complete LinearDeprecation

--- src/uk/ac/ed/bikerental/LinearDeprecation.java
+++ src/uk/ac/ed/bikerental/LinearDeprecation.java
@@ -8,24 +8,22 @@ public class LinearDeprecation implements ValuationPolicy {
 
     public BigDecimal calculateValue(Bike bike, LocalDate date) {
 
+        // Period is number of years
         LocalDate today = LocalDate.now();
         Period period = Period.between(date, today);
 
         // Original value of the bike
         BigDecimal replacementVal = bike.getBikeType().getReplacementValue();
 
-        // 
+        // Deprecation rate chosen by the designer
         double deprecationRate = 3;
         
+        // replacementVal = replacementVal - replacementVal*(deprecationRate/100)*(Num. of years)
         replacementVal =  replacementVal.subtract(
             replacementVal.multiply(BigDecimal.valueOf(deprecationRate/100)).multiply(
                 BigDecimal.valueOf(  period.getYears() )));
 
         bike.getBikeType().setReplacementValue(replacementVal);
-
-        
-
-
 
         return new BigDecimal(10);
     }
